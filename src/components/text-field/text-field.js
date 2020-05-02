dce375b6ef4a3f6e2730e660920b78757069920a
Alyona <alyona.dux@yandex.ru>
2020-05-02 16:04:17 +0300

    добавлен masked-text-field

---
+++ src/components/text-field/text-field.js
@@ -0,0 +1,3 @@
+$(document).ready(() => {
+  $(".js-text-field_masked").mask("99.99.9999")
+});
---
+++ src/pages/jquery-for-parcel.js
@@ -0,0 +1,3 @@
+var jquery = require('../../node_modules/jquery/dist/jquery.js');
+window.$ = window.jQuery = jquery;
+require('../../node_modules/jquery.maskedinput/src/jquery.maskedinput.js');

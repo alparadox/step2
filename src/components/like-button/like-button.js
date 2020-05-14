aeae299a9df4bf33623ca0527e64be38f3c0979a
Alyona <alyona.dux@yandex.ru>
2020-05-14 17:28:27 +0300

    Изменен like-button
    добавлено разграничение между кнопками

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -1,30 +1,34 @@
 $(document).ready(() => {
-  $('.like-button').click(function () {
+  $(".like-button").click(function () {
+    console.log($(this));
 
-    if ($('.like-button').hasClass('like-button_checked')) {
-      $('.like-button').removeClass('like-button_checked');
+    let $button = $(this);
+    let $number = $button.find(".like-button__number");
+    let $icon = $button.find(".like-button__icon");
+
+    if ($button.hasClass("like-button_checked")) {
+      $button.removeClass("like-button_checked");
     } else {
-      $('.like-button').addClass('like-button_checked');
+      $button.addClass("like-button_checked");
     }
 
-    let likeCount = +$('.like-button__number').html();
-    if ($('.like-button__number').hasClass('like-button__number_checked')) {
-      $('.like-button__number').removeClass('like-button__number_checked');
+    let likeCount = +$number.html();
+    if ($number.hasClass("like-button__number_checked")) {
+      $number.removeClass("like-button__number_checked");
       likeCount = likeCount - 1;
     } else {
-      $('.like-button__number').addClass('like-button__number_checked');
+      $number.addClass("like-button__number_checked");
       likeCount = likeCount + 1;
     }
 
-    $('.like-button__number').html(likeCount);
+    $number.html(likeCount);
 
-    if ($('.like-button__icon').hasClass('like-button__icon_checked')) {
-      $('.like-button__icon').removeClass('like-button__icon_checked');
-      $('.like-button__icon').html('favorite_border');
+    if ($icon.hasClass("like-button__icon_checked")) {
+      $icon.removeClass("like-button__icon_checked");
+      $icon.html("favorite_border");
     } else {
-      $('.like-button__icon').addClass('like-button__icon_checked');
-      $('.like-button__icon').html('favorite');
+      $icon.addClass("like-button__icon_checked");
+      $icon.html("favorite");
     }
-
   });
 });

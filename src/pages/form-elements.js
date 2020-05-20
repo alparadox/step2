42ed7d0594251a609cb65515af72e2b05a021b65
Alyona <alyona.dux@yandex.ru>
2020-05-20 14:35:36 +0300

    Добавлен expandable-checkbox-list

--- src/pages/form-elements.js
+++ src/pages/form-elements.js
@@ -2,3 +2,4 @@ import '../components/text-field/text-field.js';
 import '../components/range-slider/range-slider.js';
 import '../components/like-button/like-button.js';
 import '../components/pagination/pagination.js';
+import '../components/expandable-checkbox-list/expandable-checkbox-list.js';

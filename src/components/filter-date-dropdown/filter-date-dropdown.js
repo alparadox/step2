d63563ff5a62440646cf5a163a3376032f5de0e3
Alyona <alyona.dux@yandex.ru>
2020-05-21 21:01:51 +0300

    Изменен filter-date-dropdown

--- src/components/filter-date-dropdown/filter-date-dropdown.js
+++ src/components/filter-date-dropdown/filter-date-dropdown.js
@@ -1,5 +1,13 @@
 $(document).ready(() => {
   $('.filter-date-dropdown').datepicker({
     inline: true,
+    multipleDatesSeparator: ' - ',
+    range: true,
+    clearButton: true,
+    navTitles: {
+      days: 'MM yyyy',
+      months: 'yyyy',
+      years: 'yyyy1 - yyyy2',
+    },
   });
 });

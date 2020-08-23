987be14c2f1204e441da17937a814f69610c1290
Dmitriy <idmitriy.fsd@gmail.com>
2020-08-23 15:20:50 +0300

    Изменен header
    добавлен второй header (user)

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
   $(".header__button-toggle").click(function () {
-    $(".header__menu, .header__button-login, .header__button-register").toggleClass("header__active");
+    $(".header__menu, .header__button-login, .header__button-register, .header__user").toggleClass("header__active");
 
     let $icon = $(".header__button-toggle").html();
     if ($icon == 'dehaze') {

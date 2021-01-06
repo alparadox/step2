1837952a1a012f2669457988925ff9c4cecc2a3a
Alyona Duhnova <alyona.dux@yandex.ru>
2021-01-06 15:05:29 +0300

    Изменен search-room Добавлены оставшиеся элементы

--- src/pages/search-room.js
+++ src/pages/search-room.js
@@ -9,3 +9,4 @@ import "../components/filter-date-dropdown/filter-date-dropdown.js";
 import "../components/date-dropdown/date-dropdown.js";
 import "../components/dropdown/dropdown.js";
 import "../components/text-field/text-field.js";
+import "../components/room-card/room-card.js";

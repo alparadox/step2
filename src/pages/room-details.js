928ec7a2e3205f70cab9cedef612d623d46d765b
Alyona Duhnova <alyona.dux@yandex.ru>
2021-01-08 22:44:54 +0300

    Добавлена страница room-details

---
+++ src/pages/room-details.js
@@ -0,0 +1,12 @@
+import "../components/header/header.js";
+import "../components/footer/footer.js";
+import "../components/text-field/text-field.js";
+import "../components/range-slider/range-slider.js";
+import "../components/like-button/like-button.js";
+import "../components/pagination/pagination.js";
+import "../components/expandable-checkbox-list/expandable-checkbox-list.js";
+import "../components/filter-date-dropdown/filter-date-dropdown.js";
+import "../components/date-dropdown/date-dropdown.js";
+import "../components/dropdown/dropdown.js";
+import "../components/text-field/text-field.js";
+import "../components/price-card/price-card.js";
